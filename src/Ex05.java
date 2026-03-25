334cf7ebd8c8b3021a4b07706367906c939c7011
sthefanyvilaroucag-cell <stehfanyvilarouca.g@gmail.com>
2026-03-25 14:13:27 -0300

    feat:(Ex05)

--- src/Ex05.java
+++ src/Ex05.java
@@ -1,5 +1,19 @@
 void main() {
     Scanner sc = new Scanner(System.in);
     IO.println("meta guardar R$500 em um mês");
+    IO.println("quanto vc juntou?:");
+    int meta = 500;
+    double valorguardado = sc.nextDouble();
 
+    if (meta == valorguardado) {
+        IO.println("meta concluida");
+    }else if (valorguardado >500) {
+        IO.println("meta ultrapassada");
+        IO.println("vc ultrapassou:" + (valorguardado - meta));
+    }else {
+        IO.println("faltam:" + (meta - valorguardado));
+        IO.println("meta não atingida");
+
+
+    }
 }
