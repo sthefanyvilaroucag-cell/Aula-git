1d313cff43bd8e6db55d5fbfacc79a930d49172d
sthefanyvilaroucag-cell <sthefanyvilarouca.g@gmail.com>
2026-04-01 16:52:22 -0300

    feat:(Ex012) progresso no curso:

--- src/Ex12.java
+++ src/Ex12.java
@@ -1,3 +1,12 @@
 void main() {
-    
+    Scanner sc = new Scanner(System.in);
+    IO.println("quantos alunos no curso:");
+    int alunos = sc.nextInt();
+
+    IO.println("quantas aulas assistiu:");
+    int aulasassistidas = sc.nextInt();
+
+    IO.println("porcentual de progresso");
+    IO.println(());
+
 }
